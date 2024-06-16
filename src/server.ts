a5cc27a0e4ae105f3ef0da219286cf2b18e06cbf
Bharat Thakur <bt70696@gmail.com>
2024-06-16 20:56:08 +0530

    WEEK-18 Added rate limiting

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express, { Express } from 'express';
+import { rateLimit } from 'express-rate-limit';
 import http, { Server as HttpServer } from 'http';
 import cors from 'cors';
 import path from 'path';
@@ -37,6 +38,14 @@ export class Server {
 
     setMiddleware() {
         this.app.use(cors());
+        this.app.use(
+            rateLimit({
+                windowMs: 15 * 60 * 1000, // 15 minutes
+                limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+                standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+                legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+            })
+        );
         this.app.use(express.json());
 
         this.app.use(requestLogger);
